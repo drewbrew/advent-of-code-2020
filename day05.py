c726957b394f5d087e99edb7a68c9ed1ef6cfef1
Drew Winstel <drew@hsv.beer>
2020-12-06 06:57:13 -0600

    Improve day 5: go straight from binary to seat ID

--- day05.py
+++ day05.py
@@ -30,16 +30,25 @@ def seat_to_seat_id(seat: Tuple[int, int]) -> int:
     return seat[0] * 8 + seat[1]
 
 
+def boarding_pass_to_seat_id(boarding_pass: str) -> int:
+    """Convert a boarding pass string into seat ID
+
+    Because the strings are just binary representations of integers, we can abuse
+    Python's booleans and convert them from FBFBBFFRLR to 0101100101
+    """
+    seat_id = "".join(str(int(char in {"B", "R"})) for char in boarding_pass)
+
+    return int(seat_id, 2)
+
+
 def part_one(puzzle_input: List[str]) -> int:
     """Find the highest seat ID on the plane"""
-    return max(seat_to_seat_id(boarding_pass_to_seat(line)) for line in puzzle_input)
+    return max(boarding_pass_to_seat_id(line) for line in puzzle_input)
 
 
 def part_two(puzzle_input: List[str]) -> int:
     """Find your seat: the boarding pass where there's a gap in the sequence"""
-    seat_ids = sorted(
-        seat_to_seat_id(boarding_pass_to_seat(line)) for line in puzzle_input
-    )
+    seat_ids = sorted(boarding_pass_to_seat_id(line) for line in puzzle_input)
     for last_seat, current_seat in zip(
         seat_ids[:-1],
         seat_ids[1:],
@@ -54,6 +63,7 @@ def main():
     for boarding_pass, (row, col, seat_id) in TEST_INPUTS.items():
         assert boarding_pass_to_seat(boarding_pass) == (row, col), boarding_pass
         assert seat_to_seat_id((row, col)) == seat_id, boarding_pass
+        assert boarding_pass_to_seat_id(boarding_pass) == seat_id, boarding_pass
 
     print(part_one(REAL_INPUT))
     print(part_two(REAL_INPUT))
